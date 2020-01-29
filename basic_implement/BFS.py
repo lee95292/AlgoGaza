83848250dbacb435bb40ef37c052802af2d26c21
lee95292 <lee95292@naver.com>
2020-01-29 15:51:59 +0900

    basic listbfs

--- basic_implement/BFS.py
+++ basic_implement/BFS.py
@@ -1,18 +1,35 @@
 # 리스트로 주어진 연결리스트에서 BFS
-def listBFS():
+# 0
+# 1
+# 23
+# 45 6
+# 7 8 9
 
-    llist = []
-    visited = []
+
+def listBFS(llist):
+
+    visited = [0 for x in llist]
+    q = []
 
     def BFS(k):
         if(visited[k] == 1):
             return k
         else:
             visited[k] = 1
+            print("visit"+str(k))
 
         for i in llist[k]:
-            if(visited[k] == 1):
+            if(visited[i] == 1):
                 continue
             q.append(i)
 
+        if(len(q) == 0):
+            return
+
         BFS(q.pop(0))
+
+    BFS(0)
+
+
+listBFS([[1], [0, 2, 3], [1, 4, 5], [1, 6],
+         [2, 7], [2, 8], [6, 9], [4], [5], [6]])
