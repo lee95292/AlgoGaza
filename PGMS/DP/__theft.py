1c7d81878f9ad720621bdde92cb5c88056af8fc5
lee95292 <lee95292@naver.com>
2020-01-31 10:12:39 +0900

    solved makeN

--- PGMS/DP/__theft.py
+++ PGMS/DP/__theft.py
@@ -1,5 +1,43 @@
-def solution(money):
-    dp = []
+def solution(N, number):
+    num_set = []
+
+    num_set.append({N})
+    for i in range(2, 9):
+        lst = []
+
+        string = ''
+        for q in range(i):
+            string += str(N)
+        lst.append(int(string))
+
+        max_num = int(i / 2)
+
+        for X_index in range(0, max_num):
+            Y_index = i - X_index - 2
+
+            X = num_set[X_index]
+            Y = num_set[Y_index]
+
+            for x in X:
+                for y in Y:
+                    lst.append(x + y)
+                    lst.append(x - y)
+                    lst.append(y - x)
+                    lst.append(x * y)
+
+                    if x != 0:
+                        lst.append(int(y / x))
+                    if y != 0:
+                        lst.append(int(x / y))
+
+        lst = set(lst)
+
+        if number in lst:
+            return i
+
+        num_set.append(lst)
+
+    return -1
 
 
-print(solution([1, 2, 3, 1]))
+print(solution(5, 88))
---
+++ PGMS/DP/makeN.py
@@ -0,0 +1,46 @@
+import math
+
+
+def solution(N, number):
+    answer = 0
+    combination = [[0], [N]]
+    if(N == number):
+        return 1
+
+    for turn in range(2, 9):
+        tmpCombination = []
+
+        def extendNumber(n):
+            for i in n:
+                if(i < 1 or i > 32000):
+                    continue
+                tmpCombination.append(i)
+        for r in range(1, math.floor(turn/2)+1):
+            for k in combination[turn-r]:
+                for s in combination[r]:
+                    big = s
+                    small = k
+                    if(big < small):
+                        big, small = small, big
+                    klist = [small+big, small*big,
+                             math.floor(big/small), big-small]
+                    # if(number in klist):
+                    #     return turn
+
+                    extendNumber(klist)
+
+        nn = int(str(N)*turn)
+
+        if(nn < 32001):
+            tmpCombination.append(nn)
+
+        tmpCombination = list(set(tmpCombination))
+        combination.append(tmpCombination)
+        if(number in tmpCombination):
+            return turn
+        # print(str(turn)+":")
+        # print(combination)
+    return -1
+
+
+print(solution(8, 88))
