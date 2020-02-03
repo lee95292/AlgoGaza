22d68ac4207d2ad5ad65600d0bad8cf8662c6fd8
lee95292 <lee95292@naver.com>
2020-02-03 19:51:17 +0900

    cht biggest number

--- PGMS/sort/__biggestNumber__cheet.py
+++ PGMS/sort/__biggestNumber__cheet.py
@@ -1,12 +1,22 @@
 def solution(numbers):
+    if(sum(numbers) == 0):
+        return '0'
+
     answer = []
     numbers = sorted(numbers, key=lambda a: str(a)[0])
     snumbers = [str(a) for a in numbers]
 
-    for n in range(0, len(numbers)):
+    ln = len(numbers)
+    for n in range(0, ln):
         inst = 0
         ns = snumbers[n]
-        for a in range(0, len(answer)):  # 이진탐색으로 수정하기
+        la = len(answer)
+        st = 0
+        if(ln < 2*la):
+            if(answer[a//2] + snumbers[n] < snumbers[n]+answer[a//2]):
+                st = a//2
+
+        for a in range(st, la):  # 이진탐색으로 수정하기
             ast = answer[a]
             if(ns[0] < ast[0]):
                 continue
@@ -25,3 +35,11 @@ def solution(numbers):
 
 print(solution([3, 30, 34, 5, 9]))
 print(solution([6, 10, 2]))
+print(solution([21, 212]))
+print(solution([0, 0, 1000, 0]))
+
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
