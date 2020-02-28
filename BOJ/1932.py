2dc5fe73e282a71b6f76c86564a796ae359764cd
lee95292 <lee95292@naver.com>
2020-02-28 15:30:53 +0900

    solved boj 1932

---
+++ BOJ/1932.py
@@ -0,0 +1,16 @@
+n = input()
+n = int(n)
+
+p = []
+for i in range(n):
+    p.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    plen = len(p[i])-1
+    p[i][0] = p[i][0]+p[i-1][0]
+    p[i][plen] = p[i][plen]+p[i-1][plen-1]
+
+    for j in range(1, plen):
+        p[i][j] = p[i][j]+max(p[i-1][j-1], p[i-1][j])
+
+print(max(p.pop()))
