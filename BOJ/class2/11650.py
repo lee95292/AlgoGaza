487ffac8e947b0121c7a682920ce593fd1b2bb78
Myeonggyu Lee <lee95292@naver.com>
2022-04-21 19:32:41 +0900

    solved 11650

---
+++ BOJ/class2/11650.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+arr= []
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+
+arr.sort(key = lambda x: (x[0],x[1]))
+
+for a in arr:
+    print(*a, end=' ')
+    print()
+
+    
