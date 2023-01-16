320857639193a10bf875e6903d1ca3885a06696a
Myeonggyu Lee <lee95292@naver.com>
2023-01-17 00:34:51 +0900

    solved softeer 1204

--- others/softeer/S654.java
+++ others/softeer/S654.java
@@ -1,8 +1,14 @@
 package others.softeer;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/*
+ * 통근버스 출발순서 검증하기
+ * DP, 구간합
+ */
 public class S654 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
